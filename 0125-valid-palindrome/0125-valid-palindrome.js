2e448decdc2770769dac936cece2f25d98e2f17d
Aaron Yabut <85593147+Aaronyabut@users.noreply.github.com>
2022-11-02 20:49:21 -0700

    Time: 94 ms (81.99%), Space: 48.6 MB (23.74%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -7,30 +7,50 @@
  * E: if alphaNum is an empty string return true
  */
 var isPalindrome = function(s) {
-  // create an empty string variable
-  let alphaNum = '';
-  
-  // loop through the string
-  for (char of s) {
-    char = char.toLowerCase();
-    // if current value is a-z0-9 
-    if (/[a-z0-9]/.test(char)) {
-      // add to the empty string
-      alphaNum += char;
+  s = s.toLowerCase();
+  let first = 0;
+  let last = s.length-1;
+
+  let palindrome = true;
+
+  while (first < last) {
+    if (!/[a-z0-9]/.test(s[first])) {
+      first ++;
+    } else if (!/[a-z0-9]/.test(s[last])) {
+      last --;
+    } else if (s[first] !== s[last]) {
+      return false;
+    } else {
+      first ++;
+      last --;
     }
   }
-  
-  
-  // create a reverse string variable
-  const reverseAlphaNum = alphaNum.split('').reverse().join('')
-  // console.log('REVERSE ALPHA =', reverseAlphaNum)
-  
-  // if both strings match
-  if (alphaNum === reverseAlphaNum) {
-    // return true
-    return true;
-  }
-  
-  // return false
-  return false;
+  return palindrome
 };
+
+  // // create an empty string variable
+  // let alphaNum = '';
+  
+  // // loop through the string
+  // for (char of s) {
+  //   char = char.toLowerCase();
+  //   // if current value is a-z0-9 
+  //   if (/[a-z0-9]/.test(char)) {
+  //     // add to the empty string
+  //     alphaNum += char;
+  //   }
+  // }
+  
+  
+  // // create a reverse string variable
+  // const reverseAlphaNum = alphaNum.split('').reverse().join('')
+  // // console.log('REVERSE ALPHA =', reverseAlphaNum)
+  
+  // // if both strings match
+  // if (alphaNum === reverseAlphaNum) {
+  //   // return true
+  //   return true;
+  // }
+  
+  // // return false
+  // return false;
