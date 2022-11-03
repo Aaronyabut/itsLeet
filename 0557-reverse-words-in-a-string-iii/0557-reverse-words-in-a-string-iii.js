66216d3fac1122b037a17df8acd0bba679390662
Aaron Yabut <85593147+Aaronyabut@users.noreply.github.com>
2022-11-02 20:21:12 -0700

    Time: 132 ms (46.48%), Space: 48.8 MB (28.94%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.js
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.js
@@ -3,24 +3,35 @@
  * @return {string}
  */
 var reverseWords = function(s) {
-  let sArr = s.split(' ');
-  let revWordStr = []
+  let reverseStr = '';
   
-  for (let i = 0; i < sArr.length; i ++) {
-    let first = 0;
-    let last = sArr[i].length-1;
-    sArr[i] = sArr[i].split('')
-    while (first <= last) {
-      let firstChar = sArr[i][first];
-      let lastChar = sArr[i][last];
-      if (firstChar !== lastChar) {
-        sArr[i][first] = lastChar;
-        sArr[i][last] = firstChar;
-      }
-      first++;
-      last --;
-    }
-    revWordStr.push(sArr[i].join(''));
+  for (let i = s.length - 1; i >= 0; i --) {
+    reverseStr += s[i];
   }
-  return revWordStr.join(' ');
+  reverseStr = reverseStr.split(' ');
+  reverseStr = reverseStr.reverse();
+  reverseStr = reverseStr.join(' ');
+  return reverseStr;
 };
+// TWO POINTER //
+
+//   let sArr = s.split(' ');
+//   let revWordStr = []
+  
+//   for (let i = 0; i < sArr.length; i ++) {
+//     let first = 0;
+//     let last = sArr[i].length-1;
+//     sArr[i] = sArr[i].split('')
+//     while (first <= last) {
+//       let firstChar = sArr[i][first];
+//       let lastChar = sArr[i][last];
+//       if (firstChar !== lastChar) {
+//         sArr[i][first] = lastChar;
+//         sArr[i][last] = firstChar;
+//       }
+//       first++;
+//       last --;
+//     }
+//     revWordStr.push(sArr[i].join(''));
+//   }
+//   return revWordStr.join(' ');
