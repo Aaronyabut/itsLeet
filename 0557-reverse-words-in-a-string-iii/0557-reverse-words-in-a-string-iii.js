0ca488802ec7b39d05fd535d8822030a5827a257
Aaron Yabut <85593147+Aaronyabut@users.noreply.github.com>
2022-11-02 20:19:00 -0700

    Time: 176 ms (5.04%), Space: 48.4 MB (53.14%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.js
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.js
@@ -3,13 +3,24 @@
  * @return {string}
  */
 var reverseWords = function(s) {
-  let reverseStr = '';
+  let sArr = s.split(' ');
+  let revWordStr = []
   
-  for (let i = s.length - 1; i >= 0; i --) {
-    reverseStr += s[i];
+  for (let i = 0; i < sArr.length; i ++) {
+    let first = 0;
+    let last = sArr[i].length-1;
+    sArr[i] = sArr[i].split('')
+    while (first <= last) {
+      let firstChar = sArr[i][first];
+      let lastChar = sArr[i][last];
+      if (firstChar !== lastChar) {
+        sArr[i][first] = lastChar;
+        sArr[i][last] = firstChar;
+      }
+      first++;
+      last --;
+    }
+    revWordStr.push(sArr[i].join(''));
   }
-  reverseStr = reverseStr.split(' ');
-  reverseStr = reverseStr.reverse();
-  reverseStr = reverseStr.join(' ');
-  return reverseStr;
+  return revWordStr.join(' ');
 };
