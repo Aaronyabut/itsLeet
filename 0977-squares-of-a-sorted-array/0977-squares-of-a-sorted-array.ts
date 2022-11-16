43c2466cb2717bbed67038ab116af43601e8c381
Aaron Yabut <85593147+Aaronyabut@users.noreply.github.com>
2022-11-15 18:51:37 -0800

    Time: 1387 ms (5.20%), Space: 49.7 MB (45.15%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.ts
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.ts
@@ -1,3 +1,13 @@
+// function sortedSquares(nums: number[]): number[] {
+//   let squaredValues:number[] = [];
+  
+//   for (let num of nums) {
+//     squaredValues.push(num*num)
+//   }
+  
+//   return squaredValues.sort((a,b) => a-b);
+// };
+
 function sortedSquares(nums: number[]): number[] {
   let squaredValues:number[] = [];
   
@@ -5,5 +15,29 @@ function sortedSquares(nums: number[]): number[] {
     squaredValues.push(num*num)
   }
   
-  return squaredValues.sort((a,b) => a-b);
+  let first:number = 0;
+  let last:number = squaredValues.length-1;
+  
+  while (first <= last) {
+    let currFirst = squaredValues[first];
+    let currLast = squaredValues[last];
+    
+    if (last === first+1 && currFirst > currLast) {
+      squaredValues[last] = currFirst;
+      squaredValues[first] = currLast;
+      first ++;
+      last = squaredValues.length-1;
+    } else if (last === first+1) {
+      first ++;
+      last = squaredValues.length-1;
+    } else if (currFirst > currLast) {
+      squaredValues[last] = currFirst;
+      squaredValues[first] = currLast;
+      last--;
+    } else {
+      last --;
+    }
+  }
+  
+  return squaredValues 
 };
