0453391234de23f708553c84fc440734b70add99
Aaron Yabut <85593147+Aaronyabut@users.noreply.github.com>
2022-11-15 18:52:08 -0800

    Time: 111 ms (95.03%), Space: 49.5 MB (55.20%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.ts
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.ts
@@ -1,13 +1,3 @@
-// function sortedSquares(nums: number[]): number[] {
-//   let squaredValues:number[] = [];
-  
-//   for (let num of nums) {
-//     squaredValues.push(num*num)
-//   }
-  
-//   return squaredValues.sort((a,b) => a-b);
-// };
-
 function sortedSquares(nums: number[]): number[] {
   let squaredValues:number[] = [];
   
@@ -15,29 +5,39 @@ function sortedSquares(nums: number[]): number[] {
     squaredValues.push(num*num)
   }
   
-  let first:number = 0;
-  let last:number = squaredValues.length-1;
-  
-  while (first <= last) {
-    let currFirst = squaredValues[first];
-    let currLast = squaredValues[last];
-    
-    if (last === first+1 && currFirst > currLast) {
-      squaredValues[last] = currFirst;
-      squaredValues[first] = currLast;
-      first ++;
-      last = squaredValues.length-1;
-    } else if (last === first+1) {
-      first ++;
-      last = squaredValues.length-1;
-    } else if (currFirst > currLast) {
-      squaredValues[last] = currFirst;
-      squaredValues[first] = currLast;
-      last--;
-    } else {
-      last --;
-    }
-  }
-  
-  return squaredValues 
+  return squaredValues.sort((a,b) => a-b);
 };
+
+// function sortedSquares(nums: number[]): number[] {
+//   let squaredValues:number[] = [];
+  
+//   for (let num of nums) {
+//     squaredValues.push(num*num)
+//   }
+  
+//   let first:number = 0;
+//   let last:number = squaredValues.length-1;
+  
+//   while (first <= last) {
+//     let currFirst = squaredValues[first];
+//     let currLast = squaredValues[last];
+    
+//     if (last === first+1 && currFirst > currLast) {
+//       squaredValues[last] = currFirst;
+//       squaredValues[first] = currLast;
+//       first ++;
+//       last = squaredValues.length-1;
+//     } else if (last === first+1) {
+//       first ++;
+//       last = squaredValues.length-1;
+//     } else if (currFirst > currLast) {
+//       squaredValues[last] = currFirst;
+//       squaredValues[first] = currLast;
+//       last--;
+//     } else {
+//       last --;
+//     }
+//   }
+  
+//   return squaredValues 
+// };
