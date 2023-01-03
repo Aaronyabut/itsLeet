194ea5c7f88a39527988e24f13e9e1fb24690107
Aaron Yabut <85593147+Aaronyabut@users.noreply.github.com>
2023-01-03 13:00:29 -0800

    Time: 196 ms (30.55%), Space: 46.5 MB (32.40%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -3,27 +3,44 @@
  * @return {number}
  */
 var firstUniqChar = function(s) {
-    let queue = [];
-    let store = [];
+    let hashtable = {};
     
     for (let i = 0; i < s.length; i ++) {
-        let ind = queue.indexOf(s[i])
-        if (!store.includes(s[i])) {
-            queue.push(s[i])
-            store.push(s[i])
-        } else if (store.includes(s[i]) && !queue.includes(s[i])) {
-            continue;
+        if (!hashtable[s[i]]) {
+            hashtable[s[i]] = 1;
         } else {
-            queue.splice(ind,1)
+            hashtable[s[i]]+=1
         }
     }
     
-    let firstNonRepeat = s.indexOf(queue[0])
-    
-    if (!queue.length) {
-        return -1;
-    } else {
-        return firstNonRepeat
+    for (let j = 0; j < s.length; j ++) {
+        if (hashtable[s[j]] === 1) {
+            return j
+        }
     }
-    
+    return -1
 };
+
+//     let queue = [];
+//     let store = [];
+    
+//     for (let i = 0; i < s.length; i ++) {
+//         let ind = queue.indexOf(s[i])
+//         if (!store.includes(s[i])) {
+//             queue.push(s[i])
+//             store.push(s[i])
+//         } else if (store.includes(s[i]) && !queue.includes(s[i])) {
+//             continue;
+//         } else {
+//             queue.splice(ind,1)
+//         }
+//     }
+    
+//     let firstNonRepeat = s.indexOf(queue[0])
+    
+//     if (!queue.length) {
+//         return -1;
+//     } else {
+//         return firstNonRepeat
+//     }
+  
