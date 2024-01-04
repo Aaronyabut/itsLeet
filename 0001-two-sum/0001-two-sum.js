bcd62e628cd8b1ff6a9f64a1b1958071d755ca4b
Aaron Yabut <85593147+Aaronyabut@users.noreply.github.com>
2024-01-03 17:35:50 -0800

    Time: 214 ms (5.02%), Space: 42.2 MB (72.31%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,14 +4,18 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-  const seen = {};
-  for(let i = 0; i < nums.length; i++){
-    const firstNum = nums[i];
-    const secondNum = target - firstNum;
-    if(secondNum in seen){
-      return [seen[secondNum], i];
+  let i = 0;
+  let j = i+1;
+  
+  while (j !== undefined) {
+    let sum = nums[i] + nums[j]
+    if (sum === target) {
+      return [i,j];
+    } else if (nums[j] === undefined) {
+      i ++;
+      j = i+1;
+    } else {  
+      j ++;
     }
-    seen[firstNum] = i;
   }
-  return [0, 0];  
 };
