00dcd26e665c653e5c87ae94d556988a398e9995
Aaron Yabut <85593147+Aaronyabut@users.noreply.github.com>
2024-01-21 17:09:08 -0800

    Time: 107 ms (99.62%), Space: 58.4 MB (6.53%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -3,49 +3,15 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-  if (x < 0) return false;
-  const trueVal = x.toString();
-  const reverseVal = trueVal.split('').reverse().join('');
-  let reverseInt = '';
-  
-  for (let i = 0; i < reverseVal.length; i ++) {
-    if (/[0-9]/.test(reverseVal[i])) {
-      reverseInt += reverseVal[i]
-    }
-  }
-
-  if (trueVal === reverseInt) return true;
-  return false
+    if (x < 0) return false;
+    let y = x.toString();
+    
+    y = y.split("").reverse().join("");
+    y = Number(y);
+    
+    return x === y
+    
+    
+    
+    console.log(typeof y, "hello m8")
 };
-
-
-/**
- * @param {number} x
- * @return {boolean}
-
-var isPalindrome = function(x) {
-  // x = JSON.stringify
-  var negative = false;
-  if (x < 0) {
-    negative = true;
-  }
-  var absValue = JSON.stringify(Math.abs(x))
-  // var toString = JSON.stringify(absValue)
-  var newString = ''
-  
-  for (var i = absValue.length - 1; i > -1; i --) {
-    newString += absValue[i]
-  }
-  
-  if (negative) {
-    newString += '-'
-  }
-  newString = Number(newString)
-  
-  if (newString === x) {
-    return true;
-  }
-  return false;
-};
-
- */
